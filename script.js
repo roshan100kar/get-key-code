d3fb671ac6ae3fdf104115b0aa36af448b20945f
roshan <roshanshambharkar@rediffmail.com>
2024-01-21 16:46:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('keydown', (e) => {
+    const insert =document.getElementById('insert');
+
+    insert.innerHTML = `
+        <div class="key">
+            ${e.key}
+            <small>e.key</small>
+        </div>
+        <div class="key">
+            ${e.keyCode}
+            <small>e.keyCode</small>
+        </div>
+        <div class="key">
+            ${e.code}
+            <small>e.code</small>
+        </div>
+    `;
+})
